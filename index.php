e049468db6c3b8988e5693423dfbdd52241b2201
pwiszowaty <pawel.wiszowaty@gmail.com>
2014-05-06 13:17:39 +0200

    This was obviously essential to make it work.

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 class PLS3 {
+	
+	protected $itemsArray = '';
+	
 	public function __construct() {
+		$this->doIt();
 		exit;
+	}
+	
+	public function doIt() {
+		if(count($this->itemsArray) > 1){
+			foreach($this->itemsArray as $item) {
+				exit;
+			}
+		}
 	}
 }
 ?>
